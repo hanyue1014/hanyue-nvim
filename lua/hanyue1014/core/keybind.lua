9f3e12dffe25640051053ec47ebf249832fe4d4a
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2023-10-12 16:21:31 +0800

    Add credit

--- lua/hanyue1014/core/keybind.lua
+++ lua/hanyue1014/core/keybind.lua
@@ -17,7 +17,7 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- J or K in visual / select mode to move line up or down
+-- J or K in visual / select mode to move line up or down, thx theprimeagen
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
