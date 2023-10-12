d293b75f6b23ccfc2ddbf9943eeb07da8aa7bc28
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2023-10-12 16:00:38 +0800

    Update QoL nvim configs

--- lua/hanyue1014/core/keybind.lua
+++ lua/hanyue1014/core/keybind.lua
@@ -6,6 +6,13 @@ vim.keymap.set("i", "kj", "<Esc>", { noremap = true })
 -- default nvim file explorer (will be removing soon possibly, gonna use neotree)
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 
+-- use leader p to paste over selected content without yanking it to clipboard
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- use leader c or d when selected or not selected anything to yank the stuff into void (so won't override system clipboard)
+vim.keymap.set({"n", "v"}, "<leader>c", [["_c]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
 -- bttr line wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
@@ -13,6 +20,10 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- leader cf to use vim's default format
 vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = '[C]ode [F]ormat' })
 vim.keymap.set('v', '<leader>cf', vim.lsp.buf.format, { desc = '[C]ode [F]ormat' })
+
+-- J or K in visual / select mode to move line up or down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- In mac iterm2, remapped left option to Esc+, and is interpreted by nvim as the M key
 -- option backspace to delete word
--- lua/hanyue1014/core/vimops.lua
+++ lua/hanyue1014/core/vimops.lua
@@ -18,3 +18,7 @@ vim.o.expandtab = true
 vim.o.smartindent = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
+vim.opt.smartindent = true
+
+-- always have 8 rows below
+vim.opt.scrolloff = 8
