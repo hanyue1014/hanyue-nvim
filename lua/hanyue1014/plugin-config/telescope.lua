3291ea0a168348902c5ebf6670dfc883b64693dd
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2025-06-11 14:58:57 +0800

    Update telescope config - Rename In File command to Search file [C]ontents - Add command to search including files in gitignore and hidden files   (some that are not in the ignore list)

--- lua/hanyue1014/plugin-config/telescope.lua
+++ lua/hanyue1014/plugin-config/telescope.lua
@@ -3,7 +3,27 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "[F]ile" })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "[G]rep" })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "[B]uffer" })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "[H]elp tags" })
-vim.keymap.set('n', '<leader>fi', builtin.current_buffer_fuzzy_find, { desc = "[I]n File" })
+vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, { desc = "[C]ontents" })
+vim.keymap.set(
+    'n',
+    '<leader>fa',
+    function()
+        builtin.find_files({
+            no_ignore = true,
+            hidden = true,
+            -- some general ignore patterns that I know I would never need to access the file contents of
+            -- this list will keep expanding as I explore more stuffs
+            file_ignore_patterns = {
+                'node_modules',
+                '.git/',
+                '.venv',
+                '.DS_Store', -- avg mac experience
+                'build', -- preferrably this is the folder for built contents across ALL the languages
+            }
+        })
+    end,
+    { desc = "[A]ll Files (Including Hidden and .gitignore)" }
+)
 
 -- <C-d> in insert mode to delete buffer in buffer picker
 require('telescope').setup {
